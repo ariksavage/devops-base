9a6de1fd171d80b9e7a3e717f64f498d82f59ceb
Eric Savage <esavage@sandstormdesign.com>
2019-02-13 09:25:17 -0600

    update gulpfile for root files and placeholder fonts folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,35 @@
-'use strict';
+(()=>{
+  'use strict';
  
-const gulp         = require('gulp');
-const clear        = require('clear');
-const rename       = require('gulp-rename');
-const flatten      = require('gulp-flatten');
-const concat       = require('gulp-concat');
-const imagemin     = require('gulp-imagemin');
-const sass         = require('gulp-sass');
-    sass.compiler  = require('node-sass');
-const sassLint     = require('gulp-sass-lint');
-const autoprefixer = require('gulp-autoprefixer');
+const gulp           = require('gulp');
+const clear          = require('clear');
+const rename         = require('gulp-rename');
+const flatten        = require('gulp-flatten');
+const concat         = require('gulp-concat');
+const imagemin       = require('gulp-imagemin');
+const sass           = require('gulp-sass');
+      sass.compiler  = require('node-sass');
+const sassLint       = require('gulp-sass-lint');
+const autoprefixer   = require('gulp-autoprefixer');
 
 const paths = {
+  root: {
+    source: './build/root/**/*.*',
+    destination: './http'
+  },
   scss: {
-    source: './build/**/*.scss',
+    source: [
+      './build/**/*.scss',
+      './build/**/*.sass',
+      '!./build/root/**/*'
+    ],
     destination: './http/css'
   },
   js: {
-    source: './build/**/*.scss',
+    source: [
+      './build/**/*.js',
+      '!./build/root/**/*'
+    ],
     destination: './http/js'
   },
   images: {
@@ -28,7 +40,8 @@ const paths = {
       './build/**/*.gif',
       './build/**/*.svg',
       '!./build/**/font/**/*.svg',
-      '!./build/**/fonts/**/*.svg'
+      '!./build/**/fonts/**/*.svg',
+      '!./build/root/**/*'
     ],
     destination: 'http/img/'
   },
@@ -41,14 +54,16 @@ const paths = {
       './build/**/*.woff',
       './build/**/*.woff2',
       './build/**/font/**/*.svg',
-      './build/**/fonts/**/*.svg'
+      './build/**/fonts/**/*.svg',
+      '!./build/root/**/*'
     ],
     destination: './http/fonts/'
   },
   text: {
     source: [
       './build/**/*.txt',
-      './build/**/*.md'
+      './build/**/*.md',
+      '!./build/root/**/*'
     ],
     destination: './http/docs'
   }
@@ -56,6 +71,7 @@ const paths = {
 
 /*******************************************************************************
 * SASS
+* Compile and lint SCSS
 *******************************************************************************/
 function scss() {
   clear();
@@ -74,10 +90,11 @@ function scss() {
   .pipe(gulp.dest(paths.scss.destination));
 }
 
-gulp.task('scss', gulp.series(scss));
+gulp.task('scss', scss);
 
 /*******************************************************************************
 * JAVASCRIPT
+* Organize JavaScript. To do: lint and uglify
 *******************************************************************************/
 function js() {
   return gulp
@@ -89,6 +106,7 @@ gulp.task('js', js);
 
 /*******************************************************************************
 * IMAGES
+* Compress and organize images
 *******************************************************************************/
 function images() {
   return gulp
@@ -101,6 +119,7 @@ gulp.task('images', images);
 
 /*******************************************************************************
 * FONTS
+* Rename and organize fonts. Todo: generate webfonts, iconfont
 *******************************************************************************/
 function fonts() {
   return gulp
@@ -120,6 +139,7 @@ gulp.task('fonts', fonts );
 
 /*******************************************************************************
 * TEXT
+* Move text (txt, md) files into place as needed.
 *******************************************************************************/
 function text() {
   return gulp
@@ -127,6 +147,18 @@ function text() {
   .pipe(gulp.dest(paths.text.destination));
 }
 gulp.task('text', text);
+
+/*******************************************************************************
+* ROOT
+* Move any locally developed root files (i.e. robots.txt) into place.
+*******************************************************************************/
+
+function root() {
+  return gulp.src(paths.root.source)
+  .pipe(gulp.dest(paths.root.destination))
+}
+
+gulp.task('root', root);
 
 /*******************************************************************************
 * META
@@ -137,6 +169,8 @@ function watch() {
   gulp.watch(paths.images.source,  { usePolling: true }, images);
   gulp.watch(paths.fonts.source,   { usePolling: true }, fonts);
   gulp.watch(paths.text.source,    { usePolling: true }, text);
+  gulp.watch(paths.root.source,    { usePolling: true }, root);
 }
 
-gulp.task('default', gulp.series(scss, js, images, fonts, text, watch));
+gulp.task('default', gulp.series(scss, js, images, fonts, text, root, watch));
+})();
