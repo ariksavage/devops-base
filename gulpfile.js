2fff07773171bfb76694a6595247a3c3c438a44d
Arik Savage <ariksavage@MacBook-Pro.local>
2020-06-22 18:14:48 -0500

    update gulp, vagrant

--- gulpfile.js
+++ gulpfile.js
@@ -1,176 +1,94 @@
-(()=>{
-  'use strict';
- 
-const gulp           = require('gulp');
-const clear          = require('clear');
-const rename         = require('gulp-rename');
-const flatten        = require('gulp-flatten');
-const concat         = require('gulp-concat');
-const imagemin       = require('gulp-imagemin');
-const sass           = require('gulp-sass');
-      sass.compiler  = require('node-sass');
-const sassLint       = require('gulp-sass-lint');
-const autoprefixer   = require('gulp-autoprefixer');
+'use strict';
+const gulp = require('gulp');
 
-const paths = {
-  root: {
-    source: './build/root/**/*.*',
-    destination: './http'
-  },
-  scss: {
-    source: [
-      './build/**/*.scss',
-      './build/**/*.sass',
-      '!./build/root/**/*'
-    ],
-    destination: './http/css'
-  },
-  js: {
-    source: [
-      './build/**/*.js',
-      '!./build/root/**/*'
-    ],
-    destination: './http/js'
-  },
-  images: {
-    source: [
-      './build/**/*.jpg',
-      './build/**/*.jpeg',
-      './build/**/*.png',
-      './build/**/*.gif',
-      './build/**/*.svg',
-      '!./build/**/font/**/*.svg',
-      '!./build/**/fonts/**/*.svg',
-      '!./build/root/**/*'
-    ],
-    destination: 'http/img/'
+// list packages: npm list -g --depth=0
+
+const srcDir = './build';
+const destDir = './web';
+
+const options = {
+  style: {
+    task: 'Styles',
+    src: `${srcDir}/style/**/*.{scss,sass}`,
+    dest: `${destDir}/style`
   },
   fonts: {
-    source: [
-      './build/**/*.eot',
-      './build/**/*.ttf',
-      './build/**/*.otf',
-      './build/**/*.eot',
-      './build/**/*.woff',
-      './build/**/*.woff2',
-      './build/**/font/**/*.svg',
-      './build/**/fonts/**/*.svg',
-      '!./build/root/**/*'
-    ],
-    destination: './http/fonts/'
+    task: 'Fonts',
+    src:  `${srcDir}/fonts/**/*.{eot,otf,ttf,woff,woff2,svg}`,
+    dest: `${destDir}/fonts`
   },
-  text: {
-    source: [
-      './build/**/*.txt',
-      './build/**/*.md',
-      '!./build/root/**/*'
-    ],
-    destination: './http/docs'
+  scripts: {
+    task: 'Scripts',
+    src: [`${srcDir}/scripts/**/*.js`,`!${srcDir}/scripts/library/*.js`],
+    librarySrc: `${srcDir}/scripts/library/*.js`,
+    libraryDest: `${destDir}/scripts/library`,
+    dest: `${destDir}/scripts`
+  },
+  images: {
+    task: 'Images',
+    src: `${srcDir}/images/**/*.{png,jpg,jpeg,svg}`,
+    dest: `${destDir}/img`
+  },
+  static: {
+    task: 'Static',
+    src: `${srcDir}/static/**/*.{php,html,md,htaccess,txt}`,
+    dest: `${destDir}`
+  },
+  icons: {
+    task: 'Icons',
+    src: `${srcDir}/icons/*.svg`,
+    dest: `${destDir}/fonts`,
   }
 }
+/*******************************************************************************
+***** Creates Style and Watch: Style tasks
+*******************************************************************************/
+require('./gulp/gulp_tasks/sass.task')(gulp, options.style);
 
 /*******************************************************************************
-* SASS
-* Compile and lint SCSS
+***** Creates Fonts and Watch: Fonts tasks
 *******************************************************************************/
-function scss() {
-  clear();
-  return gulp.src(paths.scss.source)
-  .pipe(flatten())
-  .pipe(sassLint({configFile: './.scss-lint.yml'}))
-  .pipe(sassLint.format())
-  .pipe(sassLint.failOnError())
-  .pipe(sass({
-    outputStyle: 'compressed'
-  }).on('error', sass.logError))
-  .pipe(autoprefixer({
-    browsers: ['last 2 versions'],
-    cascade: true
-  }))
-  .pipe(gulp.dest(paths.scss.destination));
-}
-
-gulp.task('scss', scss);
+let styleDest = options.style.src;
+styleDest = styleDest.split('/');
+styleDest.splice(-1,1);
+styleDest = styleDest.join('/');
+styleDest = styleDest.replace('/**','');
+options.fonts.styleDest = `${styleDest}/globals`;
+options.fonts.relPath = '/fonts';
+require('./gulp/gulp_tasks/fonts.task')(gulp, options.fonts);
 
 /*******************************************************************************
-* JAVASCRIPT
-* Organize JavaScript. To do: lint and uglify
+***** Icons
 *******************************************************************************/
-function js() {
-  return gulp
-  .src(paths.js.source)
-  .pipe(flatten())
-  .pipe(gulp.dest(paths.js.destination));
-}
-gulp.task('js', js);
+options.icons.fontName = 'Base Icons';
+options.icons.formats = ['ttf', 'eot', 'woff', 'woff2', 'svg'];
+options.icons.relPath = options.fonts.relPath;
+options.icons.styleDest = options.fonts.styleDest;
+options.icons.relPath = options.fonts.relPath;
+require('./gulp/gulp_tasks/icon-font.task')(gulp, options.icons);
 
 /*******************************************************************************
-* IMAGES
-* Compress and organize images
+***** Creates Images and Watch: Images tasks
 *******************************************************************************/
-function images() {
-  return gulp
-  .src(paths.images.source)
-  .pipe(flatten())
-  .pipe(imagemin())
-  .pipe(gulp.dest(paths.images.destination));
-}
-gulp.task('images', images);
+require('./gulp/gulp_tasks/images.task')(gulp, options.images);
 
 /*******************************************************************************
-* FONTS
-* Rename and organize fonts. Todo: generate webfonts, iconfont
+***** Scripts
 *******************************************************************************/
-function fonts() {
-  return gulp
-  .src(paths.fonts.source)
-  .pipe(rename(function(font) {
-    font.dirname =  font.dirname
-                      .replace(' ','_')
-                      .toLowerCase();
-    font.basename = font.basename
-                      .replace(' - ','-')
-                      .replace(' ','_')
-                      .toLowerCase();
-  }))
-  .pipe(gulp.dest(paths.fonts.destination));
+options.scripts.fileName = 'app.js';
+options.scripts.styleDest = styleDest;
+function addNodeModuleJS(options, src) {
+  options.scripts.librarySrc.push('./node_modules/something/something.min.js');
+  return options;
 }
-gulp.task('fonts', fonts );
+// options.scripts = addNodeModuleJS(options.scripts, 'module_name/module_name.min.js');
+require('./gulp/gulp_tasks/javascript.task')(gulp, options.scripts);
 
-/*******************************************************************************
-* TEXT
-* Move text (txt, md) files into place as needed.
-*******************************************************************************/
-function text() {
-  return gulp
-  .src(paths.text.source)
-  .pipe(gulp.dest(paths.text.destination));
-}
-gulp.task('text', text);
+/**
+* Static Files
+**/
+require('./gulp/gulp_tasks/static.task')(gulp, options.static);
 
-/*******************************************************************************
-* ROOT
-* Move any locally developed root files (i.e. robots.txt) into place.
-*******************************************************************************/
-
-function root() {
-  return gulp.src(paths.root.source)
-  .pipe(gulp.dest(paths.root.destination))
-}
-
-gulp.task('root', root);
-
-/*******************************************************************************
-* META
-*******************************************************************************/
-function watch() {
-  gulp.watch(paths.scss.source,    { usePolling: true }, scss);
-  gulp.watch(paths.js.source,      { usePolling: true }, js);
-  gulp.watch(paths.images.source,  { usePolling: true }, images);
-  gulp.watch(paths.fonts.source,   { usePolling: true }, fonts);
-  gulp.watch(paths.text.source,    { usePolling: true }, text);
-  gulp.watch(paths.root.source,    { usePolling: true }, root);
-}
-
-gulp.task('default', gulp.series(scss, js, images, fonts, text, root, watch));
-})();
+gulp.task('watch', gulp.parallel('Watch: Styles', 'Watch: Fonts', 'Watch: Icons', 'Watch: Images', 'Watch: Scripts', 'Watch: Static'));
+gulp.task('build', gulp.series('Styles', 'Fonts', 'Icons', 'Images', 'Scripts', 'Static'));
+gulp.task('default', gulp.series('build', 'watch'));
